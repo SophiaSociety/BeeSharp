a51cd12dbca6d7dbb31d5b35749352b952f1ea6c
felipe-rms <felipeprb.fp@gmail.com>
2025-05-29 16:19:49 -0300

    Agora o front faz requisições para o backgit add .

--- backend/src/main/kotlin/Application.kt
+++ backend/src/main/kotlin/Application.kt
@@ -1,12 +1,23 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.http.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(CORS) {
+        anyHost() // Para desenvolvimento. Em produção, especifique os domínios permitidos!
+        allowHeader(HttpHeaders.ContentType)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowCredentials = true
+    }
     configureSerialization()
     configureSecurity()
     configureTemplating()
