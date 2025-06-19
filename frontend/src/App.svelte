a88344bdd947cd200972753bd7da20f62ab06d7e
felipe-rms <felipeprb.fp@gmail.com>
2025-06-18 21:14:32 -0300

    solving conflicts

---
+++ frontend/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Router from 'svelte-spa-router';
+    import LandingPage from './lib/LandingPage.svelte';
+    import Login from './lib/Login.svelte';
+    import Signup from './routes/Signup.svelte';
+    import HomePage from './routes/HomePage.svelte';
+    import ProfilePage from './routes/ProfilePage.svelte';
+
+    const routes = {
+        '/': LandingPage,
+        '/login': Login,
+        '/criar-conta': Signup,
+        '/albuns': HomePage,
+        '/perfil': ProfilePage
+    };
+</script>
+
+<Router {routes} />
